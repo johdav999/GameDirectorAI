f8794d48cdd03ccc0219ce46c3e72244fa25a666
johdav999 <johandavidsson@hotmail.se>
2025-08-31 18:42:56 +0200

    refactor llama runner initialization

--- Plugins/GameDirectorPlugin/Source/GameDirectorPlugin/Private/GameDirectorPlugin.cpp
+++ Plugins/GameDirectorPlugin/Source/GameDirectorPlugin/Private/GameDirectorPlugin.cpp
@@ -1,37 +1,18 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "GameDirectorPlugin.h"
-#include "Misc/Paths.h"
-#include "HAL/PlatformProcess.h"
 
 #define LOCTEXT_NAMESPACE "FGameDirectorPluginModule"
 
 void FGameDirectorPluginModule::StartupModule()
 {
-        // This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
-        StartLlamaRunner();
+    // This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
 }
 
 void FGameDirectorPluginModule::ShutdownModule()
 {
-        // This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
-        // we call this function before unloading the module.
-        if (LlamaRunnerHandle.IsValid())
-        {
-                FPlatformProcess::TerminateProc(LlamaRunnerHandle, true);
-                LlamaRunnerHandle.Reset();
-        }
-}
-
-void FGameDirectorPluginModule::StartLlamaRunner()
-{
-        const FString RunnerPath = FPaths::ConvertRelativePathToFull(FPaths::Combine(FPaths::ProjectDir(), TEXT("Binaries/Win64/llama-runner.exe")));
-        LlamaRunnerHandle = FPlatformProcess::CreateProc(*RunnerPath, nullptr, true, false, false, nullptr, 0, nullptr, nullptr);
-
-        if (!LlamaRunnerHandle.IsValid())
-        {
-                UE_LOG(LogTemp, Warning, TEXT("Failed to start Llama runner process at %s"), *RunnerPath);
-        }
+    // This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+    // we call this function before unloading the module.
 }
 
 #undef LOCTEXT_NAMESPACE
--- Plugins/GameDirectorPlugin/Source/GameDirectorPlugin/Public/GameDirectorPlugin.h
+++ Plugins/GameDirectorPlugin/Source/GameDirectorPlugin/Public/GameDirectorPlugin.h
@@ -3,20 +3,12 @@
 #pragma once
 
 #include "Modules/ModuleManager.h"
-#include "HAL/PlatformProcess.h"
 
 class FGameDirectorPluginModule : public IModuleInterface
 {
 public:
 
-	/** IModuleInterface implementation */
-	virtual void StartupModule() override;
-        virtual void ShutdownModule() override;
-
-        /** Launches the external Llama runner process */
-        void StartLlamaRunner();
-
-private:
-        /** Handle to the spawned Llama runner process */
-        FProcHandle LlamaRunnerHandle;
+/** IModuleInterface implementation */
+virtual void StartupModule() override;
+virtual void ShutdownModule() override;
 };
